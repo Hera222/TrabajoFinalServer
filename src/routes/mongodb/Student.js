e78d2a6350a00cad20e331883e14e29f78b57a4c
Hera222 <Hera222>
2024-06-21 22:28:57 +0300

    first commit

--- src/routes/mongodb/Student.js
+++ src/routes/mongodb/Student.js
@@ -16,31 +16,8 @@ const {
 router.get("/students", getStudents);
 router.get("/student/:id", getStudent);
 router.get("/studentdni/:dni", getStudentDni);
-router.post("/student", validarData, AddStudent);
-router.put("/student/:id", validarData, updateStudent);
+router.post("/student", AddStudent);
+router.put("/student/:id", updateStudent);
 router.delete("/student/:id", delStudent);
-
-/* *******************************************************  */
-/*              Sección de validación de datos              */
-/* *******************************************************  */
-
-function validarData(req, res, next) {
-  // console.log("Body....", req.body);
-  const { dni, nombre, descripcion } = req.body;
-
-  // if (!dni) {
-  //   return res.status(400).json({
-  //     message: "Ingrese un Dni válido..",
-  //     exito: false,
-  //   });
-  // }
-  // if (!nombre) {
-  //   return res.status(400).json({
-  //     message: "El nombre, del Estudiante está vacío..",
-  //     exito: false,
-  //   });
-  // }
-  next();
-}
 
 module.exports = router;
